ffdb32a7e9eaf8b168ab60cb0bddfc61da268826
lucaslobao-18 <lucaslobao14df@gmail.com>
2023-05-25 00:18:49 -0300

    Criando função de média de commits
    Co-authored-by: Gabriel da Silva Rosa <gabriel10919@outlook.com>

--- geradorrelatorio/main.py
+++ geradorrelatorio/main.py
@@ -1,14 +1,16 @@
 from gitInfo import *
+from average_commit import calculate_commit_average
 
 
 
-all_commits("allcommits")
+# all_commits("allcommits")
 
 
-email_commits("emailcommits", "lucaslobao14df@gmail.com")
+# email_commits("emailcommits", "lucaslobao14df@gmail.com")
 
 
-get_commits()
+# get_commits()
 
+calculate_commit_average()
 
-get_commits_by_user("FelipeDireito")
+# get_commits_by_user("FelipeDireito")SS
