a80003208e146093c4bb7ccaedc2a80557bee417
GZaranza <gabrielzaranza@hotmail.com>
2023-05-24 21:22:30 -0300

    função imprimindo o datafram com todos os commits com coauthors, mas por conta de uns commits antigos não roda no nosso repositorio
    Co-authored-by: Vinicius de Oliveira <githubdovinicius@gmail.com>

--- geradorrelatorio/coauthor.py
+++ geradorrelatorio/coauthor.py
@@ -2,20 +2,41 @@ from pygit2 import Repository, discover_repository
 import os
 from pygit2 import GIT_SORT_REVERSE, GIT_SORT_TIME
 from pygit2 import *
+import pygit2
+import pandas as pd
+
 
 # Abrir o repositório Git
 current_working_directory = os.getcwd()
 repository_path = discover_repository(current_working_directory)
 repository = Repository(repository_path)
 
+coauthors = []
+hashes = []
+authors = []
+columns = ['hash','author','co-author']
+
+
 # Obter o commit mais recente (HEAD)
 commit = repository.revparse_single('HEAD')
 
 # Percorrer todos os commits
 for commit in repository.walk(commit.id, GIT_SORT_TOPOLOGICAL):
-    # Comparar a mensagem do commit com a linha "Co-authored-by"
     commit_message = commit.message
     if 'Co-authored-by:' in commit_message:
-        print(f'O commit {commit.hex} tem coautores.')
-    else:
-        print(f'O commit {commit.hex} não tem coautores.')
+
+        hashes.append(str(commit.hex)[:6])
+        authors.append(commit.author.name)
+
+        lines = commit.message.splitlines()
+        for line in lines:
+            if line.startswith('Co-authored-by:'):
+                coauthors.append(line[16:].strip().split('<')[0])
+               
+    # Comparar a mensagem do commit com a linha "Co-authored-by"
+    
+    
+        #print(f'O commit {commit.hex} tem coautores.{commit.author.name} {coauthors}')
+
+df= pd.DataFrame({"authors":authors,"co-authors":coauthors},index=hashes)
+print(df)
