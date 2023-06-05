1586e54f65ade32e9dfe1343dea8badee58c0563
Gabriel da Silva Rosa <gabriel10919@outlook.com>
2023-06-04 23:14:47 -0300

    inserindo arquivo markdown com a nova reformulacao
    Co-authored-by: Catlen Oliveira <99406424+catlenc@users.noreply.github.com>

--- geradorrelatorio/verificar_tipo_arquivo.py
+++ geradorrelatorio/verificar_tipo_arquivo.py
@@ -31,6 +31,23 @@ def check_extension():
                 filename = file.filename
 
                 extension_by_author[author][extension].append(filename)
+        
+        content = '#File Extensions Report by Author\n\n'
+
+        for author, extensions in extension_by_author.items():
+            content += f'## Author: {author} \n\n'
+            content += '| Extension / Files |\n'
+            content += '| -------- | \n'
+            for extension, files in extensions.items():
+                file_list = '| \n'.join(files)
+                content += f'| **{extension}** | {file_list} | \n'
+                content += f' {file_list} | \n'
+            content += "\n"
+
+        output = 'arquivo.md'
+
+        with open(output, 'w', encoding='utf-8') as f:
+            f.write(content)
 
     except Exception as e:
         print(f'Ocorreu um erro: {e}')
