4bdf008e5279dae192b1a621ee2b8c8279a4d324
catlenc <catlen.cleane@hotmail.com>
2023-06-07 20:07:02 -0300

    implementando markdown
    Co-authored-by: Rafael Kenji <rafak.taira@gmail.com>

--- src/commit_data.py
+++ src/commit_data.py
@@ -31,7 +31,26 @@ def commit_data(date: str):
             authors.append(commit.commit.author.name)
             messages.append(commit.commit.message)
 
-    columns = ['hash', 'message', 'author']
-    df = pd.DataFrame({"message": messages, "author": authors}, index=hashes)
+    # columns = ['hash', 'message', 'author']
+    # df = pd.DataFrame({"message": messages, "author": authors}, index=hashes)
+    
+    content = '#File Commit by date\n\n'
 
-    return df
+    for author in commit_data.items():
+            content += f'## Author: {author} \n\n'
+            content += '| Author / Files |\n'
+            content += '| -------- | \n'
+    for message in commit_data.items():
+            content += f'## Messages: {message} \n\n'
+            content += '| Message / Files |\n'
+            content += '| -------- | \n'
+            content += "\n"
+
+    output = 'arquivo.md'
+
+    with open(output, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+    #return df
+
+commit_data("06-05-2023")
