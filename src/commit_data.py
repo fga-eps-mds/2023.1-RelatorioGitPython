fe0389675499bd8e8773497ac66aeb65ce60d359
Rafael Kenji <rafak.taira@gmail.com>
2023-06-13 15:59:13 -0300

    Correção da saída markdown
    Co-authored-by: Catlen Oliveira <99406424+catlenc@users.noreply.github.com>

--- src/commit_data.py
+++ src/commit_data.py
@@ -36,21 +36,20 @@ def commit_data(date: str):
     
     content = '#File Commit by date\n\n'
 
-    for author in commit_data.items():
-            content += f'## Author: {author} \n\n'
-            content += '| Author / Files |\n'
-            content += '| -------- | \n'
-    for message in commit_data.items():
-            content += f'## Messages: {message} \n\n'
-            content += '| Message / Files |\n'
-            content += '| -------- | \n'
-            content += "\n"
+    for author, message in zip(authors, messages):
+        content += f'## Author: {author} \n\n'
+        
+        content += '| -------- | \n'
+        content += f'## Messages: {message} \n\n'
+     
+        content += '| -------- | \n'
+        content += "\n"
 
-    output = 'arquivo.md'
+    output = 'arquivo_data.md'
 
     with open(output, 'w', encoding='utf-8') as f:
         f.write(content)
 
     #return df
 
-commit_data("06-05-2023")
+#commit_data("06-05-2023")
