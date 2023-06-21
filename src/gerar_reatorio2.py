079bf739f7f3f4aa9e4a7c5dc6cba30c9dc83dae
lucaslobao-18 <lucaslobao14df@gmail.com>
2023-06-21 18:40:46 -0300

    Adiciona a função tipo de arquivo ao relatório
    Co-authored-by: Catlen Oliveira <99406424+catlenc@users.noreply.github.com>

--- src/gerar_reatorio2.py
+++ src/gerar_reatorio2.py
@@ -1,17 +1,35 @@
-from gitInfo import *
-from commit_palavra import *
+#from gitInfo import *
+#from commit_palavra import *
 from verificar_tipo_arquivo import *
 from coauthor import relat_coauthor
 from average_commit import *
 import pandas as pd
 
+def gerar_relatorio():
 
-#pegando arquivo texto/md de extenção de arquivo --------
-#texto = check_extension()
-#print(texto)
+    #content = '#Relatório Geral\n\n'
 
-#coaut = relat_coauthor()
-#print(coaut)
+    #pegando arquivo texto/md de extenção de arquivo --------
+    #content += check_extension()
+    #content += texto
+    #print(content)
 
-#media_commits = calculate_commit_average()
-#print(media_commits)
+    #content += '\n\n'
+    #content += '#Relatório de Commits com Coauthor\n\n'
+    
+    coaut = relat_coauthor()
+    print(coaut)
+
+    
+    #content += '| Hash / Author / Coauthor / Data |\n'
+
+    num_linhas = len(coaut.index)
+    print(num_linhas)
+'''
+    for  in coaut:
+        content += f'| {author} / {coaut} / {date} / {hash} |\n'
+        content += '| -------- | \n'
+
+    print(content)'''  
+    #media_commits = calculate_commit_average()
+    #print(media_commits)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from commit_palavra import *
 from verificar_tipo_arquivo import *
 from coauthor import *
 from average_commit import *
+from gerar_reatorio2 import gerar_relatorio
 
 #word = commit_palavra('Criando')
 #print(word)
@@ -15,4 +16,6 @@ from average_commit import *
 
 #check_extension()
 
-calculate_commit_average()
+#calculate_commit_average()
+
+gerar_relatorio()
